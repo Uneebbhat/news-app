37a60b0a50e202a362a00b47fd6eccf7f14e19e9
Uneeb <uneebbhatti3@gmail.com>
2023-03-13 01:19:22 +0500

    completed card section now make the navbar responsive

--- js/app.js
+++ js/app.js
@@ -9,28 +9,19 @@ fetch(url)
         // Iterate over the articles and create Bootstrap cards for each one
         articles.forEach(article => {
             const card = `
-                <div class="section-wrapper">
-                    <div div class="section-card" >
-                        <div class="card-img">
-                            <img src="${article.urlToImage}"
-                                alt="">
-                        </div>
-                        <div class="card-body">
-                            <h5>${article.title}</h5>
-                            <p>${article.description}</p>
-                        </div>
-                        <div class="buttons">
-                            <a href="${article.url}" target="_blank">
-                                <button type="button" class="btn btn-primary">Read more</button>
-                            </a>
-                            <img src="img/heart.svg" alt="" onclick="likePost()">
-                        </div>
-                    </div >
-                </div>    
-            `;
+            <div class="col-md-4 mb-3">
+                <div class="card">
+                <img src="${article.urlToImage}" alt="" style="width: 100%; height: 200px; object-fit: cover;">
+                    <div class="card-body">
+                        <h5 class="card-title">${article.title}</h5>
+                        <p class="card-text" style="display: -webkit-box; -webkit-line-clamp: 3; -webkit-box-orient: vertical; overflow: hidden;">${article.description}</p>
+                        <a href="${article.url}" target="_blank" class="btn btn-primary">Read more</a>
+                    </div>
+                </div>
+            </div>
+        `;
+
             $('#news-cards').append(card);
         });
     })
     .catch(error => console.error(error));
-
-
